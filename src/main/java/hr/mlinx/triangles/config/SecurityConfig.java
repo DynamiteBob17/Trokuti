380df94b6893f380c76c0bbd10932ae75b441ecb
DynamiteBob17 <jan.mlinaric@gmail.com>
2023-08-21 19:39:35 +0200

    feat: configure spring security

---
+++ src/main/java/hr/mlinx/triangles/config/SecurityConfig.java
@@ -0,0 +1,62 @@
+package hr.mlinx.triangles.config;
+
+import hr.mlinx.triangles.entity.RoleName;
+import hr.mlinx.triangles.security.TokenAuthenticationFilter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import static org.springframework.security.web.util.matcher.AntPathRequestMatcher.antMatcher;
+
+@RequiredArgsConstructor
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    private final TokenAuthenticationFilter tokenAuthenticationFilter;
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration)
+            throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http)
+            throws Exception {
+        return http
+                .authorizeHttpRequests(auth -> auth.
+                        requestMatchers(antMatcher("/api/admin/**")).hasAnyAuthority(RoleName.ROLE_ADMIN.name())
+                        .requestMatchers(antMatcher("/api/user/**")).hasAnyAuthority(RoleName.ROLE_ADMIN.name(), RoleName.ROLE_USER.name())
+                        .requestMatchers(antMatcher("/auth/**")).permitAll()
+                        .requestMatchers(antMatcher("/error")).permitAll()
+                        .anyRequest().authenticated()
+                )
+                .logout(l -> l.logoutSuccessUrl("/").permitAll())
+                .addFilterBefore(tokenAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
+                .sessionManagement(sessionManagement -> sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .exceptionHandling(exceptionHandling -> exceptionHandling.authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED)))
+                .cors(Customizer.withDefaults())
+                .csrf(AbstractHttpConfigurer::disable)
+                .build();
+    }
+
+}
---
+++ src/main/java/hr/mlinx/triangles/security/UserDetailsServiceImpl.java
@@ -0,0 +1,36 @@
+package hr.mlinx.triangles.security;
+
+import hr.mlinx.triangles.entity.User;
+import hr.mlinx.triangles.exception.ResourceNotFoundException;
+import hr.mlinx.triangles.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.hibernate.Hibernate;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+@Transactional
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserService userService;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userService.getUserByUsername(username)
+                .orElseThrow(() -> new ResourceNotFoundException("user", "username", username));
+
+        CustomUserDetails customUserDetails = new CustomUserDetails();
+        customUserDetails.setId(user.getId());
+        customUserDetails.setUsername(user.getUsername());
+        customUserDetails.setPassword(user.getPassword());
+        Hibernate.initialize(userService.getRolesOfUser(user));
+        customUserDetails.setRoles(userService.getRolesOfUser(user));
+
+        return customUserDetails;
+    }
+
+}
